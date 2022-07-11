15ccbbae27552560d9875564755c02973ea674fd
the-sneg <the.sneg.sasha@gmail.com>
2022-07-11 15:08:00 +0300

    edd

--- js/warning_btn.js
+++ js/warning_btn.js
@@ -1,1 +1,8 @@
 import Notiflix from 'notiflix';
+
+const warBtn = document.querySelector('#warning');
+warBtn.addEventListener('click', warMess);
+
+function warMess() {
+  return Notiflix.Notify.warning('wwww');
+}
