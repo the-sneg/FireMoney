064b5ea7cd50f54ea99f48f6c838e27153e463de
the-sneg <the.sneg.sasha@gmail.com>
2022-07-11 15:53:23 +0300

    add notiflix

--- js/warning_btn.js
+++ js/warning_btn.js
@@ -1,8 +1,7 @@
-// import Notiflix from '../node_modules/notiflix/src/notiflix';
-
-const warBtn = document.querySelector('[warning]');
-warBtn.addEventListener('click', warMess);
-
-function warMess() {
-  return alert('Это тестовая версия! Некоторые функции не доступны!');
-}
+const warBtn = document.querySelectorAll('[warning]');
+warBtn.forEach(function (e) {
+  e.onclick = function () {
+    event.preventDefault();
+    return Notiflix.Notify.failure('Это тестовая версия! Некоторые функции не доступны!');
+  };
+});
